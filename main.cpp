80b5df15e06adab9bf572c4422248a928915bced
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-29 04:16:57 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+void astrologySignCalculator(int day, string month)
+{
+  if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else if(month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  else (month == "Jan") {
+  if(day < 21)
+  {
+    cout << "Capricorn" << endl;
+  }
+    else
+  {
+    cout << "Aquarius" << endl;
+  }
+  }
+
+  
+}
+
+int main() {
+
+  int t;
+    cin >> t;
+
+  for(int i = 0; i < t; i ++)
+  {
+   string date;
+    cin  >> date;
+
+    astrologySignCalculator(date); // day and month which are two variables unlike those in the void
+      
+  }
+}
